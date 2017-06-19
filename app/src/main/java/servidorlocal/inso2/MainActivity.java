559541bcaae551eede7fbfa6d18e186c05c670ae
zifherx <frojasq@yahoo.com>
2017-06-19 11:59:12 -0500

    Creación Proyecto
    Interfaz Login

---
+++ app/src/main/java/servidorlocal/inso2/MainActivity.java
@@ -0,0 +1,13 @@
+package servidorlocal.inso2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
